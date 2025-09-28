0fdbdbaed356ad3fb6137d5912c247f014c7335f
Ritin <ritinrajore26@gmail.com>
2025-09-28 18:15:51 +0000

    New files

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const AccountId = 1234
+let accountEmail = "ritin@gmail.account"
+var accountPassword = "1212"
+accountCity = "Delhi"
+let accountstate; 
+
+// AccountId = 2 not allowed 
+
+accountEmail = "RR.com"
+accountPassword = "3233"
+accountCity = "Noida"
+
+/* 
+Prefer not use var
+because of issue in block scope and functional scope 
+*/
